a8ff04bb13281831f9875aeec3afb63cde6b7b53
Eoghaners <stiger.home@gmail.com>
2021-12-29 22:10:55 +0000

    Added Code to Enable Movement of Player

--- Godot/Projects/Tutorials/action-RPG/Player/player.gd
+++ Godot/Projects/Tutorials/action-RPG/Player/player.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2.ZERO
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = 4
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -4
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y = -4
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = 4
+	else:
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_collide(velocity)
